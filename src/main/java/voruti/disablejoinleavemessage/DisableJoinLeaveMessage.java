ba514f033d78dbc33447e8c82b3f74fbfa788c1a
voruti <45130096+voruti@users.noreply.github.com>
2022-06-29 21:18:12 +0200

    JavaDoc
    (I think this project is way too small for a package-info.java xD.)

--- src/main/java/voruti/disablejoinleavemessage/DisableJoinLeaveMessage.java
+++ src/main/java/voruti/disablejoinleavemessage/DisableJoinLeaveMessage.java
@@ -6,9 +6,15 @@ import org.apache.logging.log4j.Logger;
 
 public class DisableJoinLeaveMessage implements ModInitializer {
 
+    /**
+     * Logger used for all log messages from this mod.
+     */
     public static final Logger LOGGER = LogManager.getLogger(
             DisableJoinLeaveMessage.class);
 
+    /**
+     * Announce mod startup.
+     */
     @Override
     public void onInitialize() {
         LOGGER.info("DisableJoinLeaveMessage initialized");
