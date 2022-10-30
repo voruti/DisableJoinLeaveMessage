c9c1541aff5af67494a0422f0109dfd3457dd1b7
voruti <45130096+voruti@users.noreply.github.com>
2022-10-30 14:14:41 +0100

    Fix mixins

--- src/main/java/voruti/disablejoinleavemessage/mixin/PlayerManagerMixin.java
+++ src/main/java/voruti/disablejoinleavemessage/mixin/PlayerManagerMixin.java
@@ -6,8 +6,8 @@ import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.registry.RegistryKey;
 import org.apache.logging.log4j.Level;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,35 +15,36 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import voruti.disablejoinleavemessage.DisableJoinLeaveMessage;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
 
     @Inject(method = "broadcast(Lnet/minecraft/text/Text;"
             + "Ljava/util/function/Function;"
-            + "Lnet/minecraft/util/registry/RegistryKey;)V",
+            + "Z)V",
             at = @At("HEAD"),
             cancellable = true)
     private void broadcastAlternative(final Text message,
                                       final Function<ServerPlayerEntity, Text>
                                               playerMessageFactory,
-                                      final RegistryKey<MessageType> typeKey,
+                                      final boolean overlay,
                                       final CallbackInfo ci) {
         logic(message, ci);
     }
 
     @Inject(method = "broadcast(Lnet/minecraft/network/message/SignedMessage;"
-            + "Ljava/util/function/Function;"
-            + "Lnet/minecraft/network/message/MessageSender;"
-            + "Lnet/minecraft/util/registry/RegistryKey;)V",
+            + "Ljava/util/function/Predicate;"
+            + "Lnet/minecraft/server/network/ServerPlayerEntity;"
+            + "Lnet/minecraft/network/message/MessageSourceProfile;"
+            + "Lnet/minecraft/network/message/MessageType$Parameters;)V",
             at = @At("HEAD"),
             cancellable = true)
     private void broadcastAlternative(final SignedMessage message,
-                                      final Function<ServerPlayerEntity,
-                                              SignedMessage>
-                                              playerMessageFactory,
-                                      final MessageSourceProfile sender,
-                                      final RegistryKey<MessageType> typeKey,
+                                      final Predicate<ServerPlayerEntity> shouldSendFiltered,
+                                      final @Nullable ServerPlayerEntity sender,
+                                      final MessageSourceProfile sourceProfile,
+                                      final MessageType.Parameters params,
                                       final CallbackInfo ci) {
         logic(message.getContent(), ci);
     }
